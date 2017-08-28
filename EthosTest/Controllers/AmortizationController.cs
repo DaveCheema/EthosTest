eec5c0ca384dfd0120349eed37439148cfce964d
dave cheema <dcheema@socal.rr.com>
2017-08-27 19:51:25 -0700

    Add project files.

---
+++ EthosTest/Controllers/AmortizationController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using EthosTest.Models;
+using EthosTest.Engines;
+
+namespace EthosTest.Controllers
+{
+    // AmortizationController is the central focal point for loan amortization model.    
+    public class AmortizationController : Controller
+    {
+        // GET: Amortization
+        public ActionResult Amortizer()
+        {
+            
+            // Create a data model instance.
+            var amortizationModel = new AmortizationModel();
+
+            // Create a tuple to pass multiple data models.
+            var tuple = new Tuple<AmortizationModel, List<MonthlyPaymentDetail>>
+                (amortizationModel, amortizationModel.MonthlyPaymentDetails);
+
+            return View(tuple);
+        }
+
+        // Accepts user entered parameters, generates amortization model and presents to the user.
+        [HttpPost]
+        public ActionResult Amortizer(AmortizationModel amortizationModel)
+        {
+
+            // If no validation errors, create the amortization model.
+            if (ModelState.IsValid)
+            {                
+                amortizationModel =
+                    GetPaymentModel(  amortizationModel.LoanAMount
+                                    , amortizationModel.LoanTermInMonths
+                                    , amortizationModel.InterestRate)
+                    .Result;
+               
+                ModelState.Clear();
+            }
+
+            //Tuple is used to pass multiple data models.
+            var tuple = new Tuple<AmortizationModel, List<MonthlyPaymentDetail>>
+                        (amortizationModel, amortizationModel.MonthlyPaymentDetails);
+
+            return View(tuple);
+
+        }
+
+        // GetPaymentModel is called asynchronously to avoid any blocking for improved performance. 
+        private async static Task<AmortizationModel> GetPaymentModel( double loanAMount
+                                                                    , short loanTermInMonths
+                                                                    , double interestRate)
+        {
+            // Generate amortization model asynchronously.
+            return
+                 await 
+                 AmortizationEngine.GeneratePaymentModel( loanAMount
+                                                        , loanTermInMonths
+                                                        , interestRate);
+        }
+    }
+}
---
+++ EthosTest/Controllers/WordTwisterController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EthosTest.Models;
+using EthosTest.Engines;
+
+namespace EthosTest.Controllers
+{
+    // WordTwisterController is used to twister user entered phrases based on user selected twist option.   
+    public class WordTwisterController : Controller
+    {
+        // GET: WordTwister
+        public ActionResult WordTwister()
+        {
+            return View();
+        }
+
+        // Receives a phrase and a twist option from the user and provides the twisted phrase.
+        [HttpPost]
+        public ActionResult WordTwister(WordTwisterModel twister)
+        {
+            if (ModelState.IsValid)
+            {
+                twister.Text = TwistIt(twister);
+                ModelState.Clear();
+            }
+
+            return View(twister);
+
+        }
+
+        // Twist user entered phrase based on user selected option.
+        private static string TwistIt(WordTwisterModel twister)
+        {
+            string twistedPhrase = null;
+
+            // Produce result based on Twist Action selecteed.
+            switch (twister.TwistAction)
+            {
+                case Twist.ReverseWordOrder:
+                    twistedPhrase = WordTwisterEngine.ReverseWordOrder(twister.Text);
+                    break;
+                case Twist.ReverseCharacters:
+                    twistedPhrase = WordTwisterEngine.ReverseCharacters(twister.Text);
+                    break;
+                case Twist.SortWordsAlphabetically:
+                    twistedPhrase = WordTwisterEngine.SortWordsAlphabetically(twister.Text);
+                    break;
+                case Twist.EncryptInput:
+                    // .Result is used to extract the result.
+                    twistedPhrase = WordTwisterEngine.EncryptInput(twister.Text).Result;    
+                    break;
+            }
+
+            return twistedPhrase;
+        }
+    }
+}
